a67f7634708310267ed4b444f8593004d4c9087c
Jessica Kerr <jessitron@atomist.com>
2019-06-24 16:59:30 -0400

    Cleanup

--- views/org.tsx
+++ views/org.tsx
@@ -57,21 +57,8 @@ function displayImportantFeature(f: FeatureForDisplay, i: number): React.ReactEl
     const key = "collapsible" + i;
     const expandByDefault = f.fingerprints.length <= 1;
 
-    // <div class="wrap-collabsible">
-    //     <input id="collapsible" class="toggle" type="checkbox">
-    //         <label for="collapsible" class="lbl-toggle">More Info</label>
-    //         <div class="collapsible-content">
-    //             <div class="content-inner">
-    //                 <p>
-    //                     QUnit is by calling one of the object that are embedded in JavaScript, and faster JavaScript program could also used with
-    //                     its elegant, well documented, and functional programming using JS, HTML pages Modernizr is a popular browsers without
-    //                     plug-ins. Test-Driven Development.
-    //       </p>
-    //             </div>
-    //         </div>
-    // </div>
     return <div className="wrap-collabsible">
-        <input id={key} className="toggle" type="checkbox" checked={expandByDefault}></input>
+        <input id={key} className="sneaky toggle" type="checkbox" checked={expandByDefault}></input>
         <label htmlFor={key} className="lbl-toggle">{f.feature.displayName} ({f.fingerprints.length})</label>
         <div className="collapsible-content">
             <div className="content-inner">
