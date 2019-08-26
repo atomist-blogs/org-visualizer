dce6b4c0ed9abddd8baae31931da4f9100d3b095
Garen J. Torikian <gjtorikian@gmail.com>
2019-08-26 21:31:07 +0000

    Autofix: tslint
    [atomist:generated] [atomist:autofix=tslint]

--- test/atomist.config.ts
+++ test/atomist.config.ts
@@ -53,7 +53,6 @@ import {
     MavenDefaultOptions,
 } from "@atomist/sdm-pack-spring";
 import * as _ from "lodash";
-import { NodeEngine } from "../lib/aspect/community/nodeEngine";
 import { PostgresProjectAnalysisResultStore } from "../lib/analysis/offline/persist/PostgresProjectAnalysisResultStore";
 import {
     CombinationTagger,
@@ -68,6 +67,7 @@ import {
     License,
     LicensePresence,
 } from "../lib/aspect/community/license";
+import { NodeEngine } from "../lib/aspect/community/nodeEngine";
 import {
     ChangelogAspect,
     ContributingAspect,
